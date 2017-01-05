83a1bebf6a4bee38fa38f0f794d23c3eaefbe3a3
moens-PC\moens <mones0008@gmail.com>
2017-01-05 18:50:45 +0600

    add essential file for heroku

---
+++ index.js
@@ -0,0 +1,26 @@
+var http =require('http');
+var express = require('express');
+var os =require('os');
+
+var app =express();
+
+app.set('port',(process.env.PORT || 5000))
+
+app.get('/',function(req,res){
+    res.setHeader("Content-Type","application/json");
+    var ipInfo =req.headers.host;
+    var langInfo =req.headers['accept-language'].split(',')[0];
+    var softwareInfo = req.headers['user-agent'];
+    softwareInfo =  softwareInfo.slice(softwareInfo.indexOf('(')+1,softwareInfo.indexOf(')'));  
+    var jsonObj ={
+        'ipaddress':ipInfo,
+        'language':langInfo,
+        'software':softwareInfo,
+    } 
+    res.send(JSON.stringify(jsonObj));
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
